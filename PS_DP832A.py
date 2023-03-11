61b8242bc2b677f8d556565178b7fb92bf1ec630
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2023-03-11 10:02:35 +0500

    Рефакторинг

--- PS_DP832A.py
+++ PS_DP832A.py
@@ -1,4 +1,5 @@
 # Author: Malyshev Sergey 2022, Ekaterinburg
+# https://github.com/sergey12malyshev
 #
 # References
 #  - http://www.batronix.com/pdf/Rigol/ProgrammingGuide/DP800_ProgrammingGuide_EN.pdf
@@ -22,8 +23,9 @@ except:
   
 #sg.theme_previewer() # Просмотр всех доступных тем
 
-#--------------------------GLOBAL VARIABLES------------------------------------------
-delayAfterMeasurement = 0.01
+#--------------------------GLOBAL VARIABLES CONST---------------------------------
+DELAY_AFTER_MEAS = 0.01
+DEBUG = False
 
 #--------------------------GENERAL CLASS------------------------------------------
 # https://proglib.io/p/python-oop
@@ -53,58 +55,40 @@ class Canal_DP832(object): # Создали класс
         psu.write(f":OUTP CH{channel},OFF") # disable the output of channel
         print(f"channel {channel} DP832A disable")
         window['quote'].update(f'Output {channel} disable')
-        
-    def off_all_channel(self):
-        self.off_channel(1);
-        self.off_channel(2);
-        self.off_channel(3);
-        window['quote'].update('Output all disable')
-   
+           
 ch1 = Canal_DP832(24, 0.2, 33, 3) # Экземпляры классов(объекты)
 ch2 = Canal_DP832(24, 0.3, 33, 3) # Установить значения по умолчанию
 ch3 = Canal_DP832(5, 0.3, 5.5, 0.5)
 
-
-layout =  [ [sg.Frame('CH1', [[sg.Button('Set CH1'), sg.Button('Reset CH1'), sg.Text(f'{ch1.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_1-', text_color='yellow'), sg.Text(f'{ch1.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_1-', text_color='yellow')], 
-            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch1.voltage}', size=(4, 1), font=('Helvetica 11'), key='voltage_out'), sg.InputText(key='-VOLTAGE-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch1.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out'), sg.InputText(key='-OVP-', size=(6, 1))],
-            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch1.current}', size=(4, 1), font=('Helvetica 11'), key='current_out'), sg.InputText(key='-CURRENT-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch1.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out'), sg.InputText(key='-OCP-', size=(6, 1))]])],
-            
-            [sg.Frame('CH2', [[sg.Button('Set CH2'), sg.Button('Reset CH2'), sg.Text(f'{ch2.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_2-', text_color = 'cyan'), sg.Text(f'{ch2.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_2-', text_color = 'cyan')], 
-            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch2.voltage}', size=(4, 1), font=('Helvetica 11'), key='voltage_out2'), sg.InputText(key='-VOLTAGE2-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch2.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out2'), sg.InputText(key='-OVP2-', size=(6, 1))],
-            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch2.current}', size=(4, 1), font=('Helvetica 11'), key='current_out2'), sg.InputText(key='-CURRENT2-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch2.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out2'), sg.InputText(key='-OCP2-', size=(6, 1))]])],
-            
-            [sg.Frame('CH3', [[sg.Button('Set CH3'), sg.Button('Reset CH3'), sg.Text(f'{ch3.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_3-', text_color = 'magenta'), sg.Text(f'{ch3.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_3-', text_color = 'magenta')], 
-            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch3.voltage} ', size=(4, 1), font=('Helvetica 11'), key='voltage_out3'), sg.InputText(key='-VOLTAGE3-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch3.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out3'), sg.InputText(key='-OVP3-', size=(6, 1))],
-            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch3.current}', size=(4, 1), font=('Helvetica 11'), key='current_out3'), sg.InputText(key='-CURRENT3-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch3.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out3'), sg.InputText(key='-OCP3-', size=(6, 1))]])],
-            
-            [sg.Frame('Fast preset', [[sg.Button('CH1'), sg.Button('CH2'), sg.Button('CH3'), sg.Button('OFF')]]), sg.Frame('System', [[sg.Button('Exit', size=(5, 1)), sg.Button('About', size=(5, 1))]])],
-            [sg.Text('This is GUI driving Rigol DP832A', key='quote')]
-         ]
-
-#--------------------------GENERAL FUNCTIONS-----------------------------------------
+#--------------------------GENERAL FUNCTIONS---------------------------------------
+def off_all_channel():
+    ch1.off_channel(1);
+    ch2.off_channel(2);
+    ch3.off_channel(3);
+    window['quote'].update('Output all disable')
+        
 def measVolt(chan): 			
     cmd1 = ':MEAS:VOLT? CH%s' %chan
     V = psu.query(cmd1)
     V = float(V)
-    time.sleep(delayAfterMeasurement)
+    time.sleep(DELAY_AFTER_MEAS)
     return V
 
 def measCurrent(chan): 			
     cmd1 = ':MEAS:CURR? CH%s' %chan
     C = psu.query(cmd1)
     C = float(C)
-    time.sleep(delayAfterMeasurement)
+    time.sleep(DELAY_AFTER_MEAS)
     return C
 
 def measPower(chan): 			
     cmd1 = ':MEAS:POWE? CH%s' %chan
     P = psu.query(cmd1)
     P = float(P)
-    time.sleep(delayAfterMeasurement)
+    time.sleep(DELAY_AFTER_MEAS)
     return P
 
 def mainMeas(): 
-
     def debugOut(): 			
         print("Power: " + str(ch1.power) + " mW" + "    Voltage: " + str(ch1.volt) + " V" + "    Current: " + str(ch1.current) + " A")
         print("Power: " + str(ch2.power) + " mW" + "    Voltage: " + str(ch2.volt) + " V" + "    Current: " + str(ch2.current) + " A")
@@ -122,7 +106,7 @@ def mainMeas():
     ch3.measVolt = round(measVolt(3), 2)
     ch3.measCurrent = round(measCurrent(3), 4)
     
-    #debugOut ()
+    if DEBUG: debugOut()
     
 def screenUpdateValue():
     window['-OUTPUT_VOLT_1-'].update(str(ch1.measVolt) + " V") 
@@ -132,16 +116,30 @@ def screenUpdateValue():
     window['-OUTPUT_VOLT_3-'].update(str(ch3.measVolt) + " V")  
     window['-OUTPUT_CURR_3-'].update(str(ch3.measCurrent) + " A")    
   
-#--------------------------------MAIN------------------------------------------------
- 
+#--------------------------------GUI------------------------------------------------
+layout =  [ [sg.Frame('CH1', [[sg.Button('Set CH1'), sg.Button('Reset CH1'), sg.Text(f'{ch1.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_1-', text_color='yellow'), sg.Text(f'{ch1.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_1-', text_color='yellow')], 
+            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch1.voltage}', size=(4, 1), font=('Helvetica 11'), key='voltage_out'), sg.InputText(key='-VOLTAGE-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch1.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out'), sg.InputText(key='-OVP-', size=(6, 1))],
+            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch1.current}', size=(4, 1), font=('Helvetica 11'), key='current_out'), sg.InputText(key='-CURRENT-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch1.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out'), sg.InputText(key='-OCP-', size=(6, 1))]])],
+            
+            [sg.Frame('CH2', [[sg.Button('Set CH2'), sg.Button('Reset CH2'), sg.Text(f'{ch2.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_2-', text_color = 'cyan'), sg.Text(f'{ch2.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_2-', text_color = 'cyan')], 
+            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch2.voltage}', size=(4, 1), font=('Helvetica 11'), key='voltage_out2'), sg.InputText(key='-VOLTAGE2-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch2.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out2'), sg.InputText(key='-OVP2-', size=(6, 1))],
+            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch2.current}', size=(4, 1), font=('Helvetica 11'), key='current_out2'), sg.InputText(key='-CURRENT2-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch2.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out2'), sg.InputText(key='-OCP2-', size=(6, 1))]])],
+            
+            [sg.Frame('CH3', [[sg.Button('Set CH3'), sg.Button('Reset CH3'), sg.Text(f'{ch3.measVolt}', size=(6, 1), font=('Helvetica', 16), key='-OUTPUT_VOLT_3-', text_color = 'magenta'), sg.Text(f'{ch3.measCurrent}', size=(7, 1), font=('Helvetica', 16), key='-OUTPUT_CURR_3-', text_color = 'magenta')], 
+            [sg.Text('Voltage, V:', size=(8, 1)), sg.Text(f'{ch3.voltage} ', size=(4, 1), font=('Helvetica 11'), key='voltage_out3'), sg.InputText(key='-VOLTAGE3-', size=(6, 1)), sg.Text('OVP:', size=(4, 1)), sg.Text(f'{ch3.ovp}', size=(2, 1), font=('Helvetica 11'), key='OVP_out3'), sg.InputText(key='-OVP3-', size=(6, 1))],
+            [sg.Text('Current, A:', size=(8, 1)), sg.Text(f'{ch3.current}', size=(4, 1), font=('Helvetica 11'), key='current_out3'), sg.InputText(key='-CURRENT3-', size=(6, 1)), sg.Text('OCP:', size=(4, 1)), sg.Text(f'{ch3.ocp}', size=(2, 1), font=('Helvetica 11'), key='OCP_out3'), sg.InputText(key='-OCP3-', size=(6, 1))]])],
+            
+            [sg.Frame('Fast preset', [[sg.Button('CH1'), sg.Button('CH2'), sg.Button('CH3'), sg.Button('OFF')]]), sg.Frame('System', [[sg.Button('Exit', size=(5, 1)), sg.Button('About', size=(5, 1))]])],
+            [sg.Text('This is GUI driving Rigol DP832A', key='quote')]
+         ] 
 #STEP 2 - create the window
 window = sg.Window('Run DP832A', layout)
 
-# STEP3 - the event loop
+#STEP3 - the event loop
 while True:
   
     event, values = window.read(timeout=200)   # Read the event that happened and the values dictionary, timeout=200 - не блокирующий режим  
-    #print(event, values) #DEBUG
+    if DEBUG: print(event, values)
     
     if event == sg.WIN_CLOSED or event == 'Exit':     # If user closed window with X or if user clicked "Exit" button then exit
         break
